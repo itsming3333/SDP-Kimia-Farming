3f445de947df1c756fb8c39f420b5bf96880426d
Fu Ming <fuming308@yahoo.co.id>
2019-09-29 16:47:06 +0700

    form pengguna dan coba commit

---
+++ KimiaFarming/FormParent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KimiaFarming
+{
+    public partial class FormParent : Form
+    {
+        FormPengguna f_pengguna;
+        public FormParent()
+        {
+            InitializeComponent();
+            f_pengguna = new FormPengguna();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            f_pengguna.MdiParent = this;
+            f_pengguna.Location = new Point(0, 0);
+            f_pengguna.Show();
+        }
+    }
+}
